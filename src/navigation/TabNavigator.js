d04f831a01523be7385c2522bb2cd89f68c35918
A-K0173 <n01730789@humber.ca>
2025-11-27 18:34:22 -0500

    finalize

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,5 +1,8 @@
+import Icon from '@react-native-vector-icons/material-icons';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+import SearchScreen from '../screens/SearchScreen';
+import SettingsScreen from '../screens/SettingsScreen';
 import StackNavigator from './StackNavigator';
 
 const Tab = createBottomTabNavigator();
@@ -23,7 +26,30 @@ const TabNavigator = () => {
         component={StackNavigator}
         options={{
           title: 'Home',
-          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+          headerShown: false, // Hide header since StackNavigator has its own
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          title: 'Search',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="search" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="settings" color={color} size={size} />
+          ),
         }}
       />
     </Tab.Navigator>
