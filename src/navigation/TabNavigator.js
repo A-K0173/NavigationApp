5bc96544673caf1fd744524b942531ccd7ae3e02
A-K0173 <n01730789@humber.ca>
2025-11-27 18:13:32 -0500

    Tab Navigator creation while i figure this sout

---
+++ src/navigation/TabNavigator.js
@@ -0,0 +1,9 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return <Tab.Navigator></Tab.Navigator>;
+};
+
+export default TabNavigator;
